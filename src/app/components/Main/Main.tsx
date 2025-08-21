3f2f99e1f89c523ef6a808db56a02e74217a029a
Patrick <patrickyoel13@gmail.com>
2025-08-21 09:24:19 -0300

    Definicion de los componentes principales.

---
+++ src/app/components/Main/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SectionA } from './SectionA';
+
+export const Main: React.FC = () => {
+    return (
+        <main>
+            <SectionA />
+        </main>
+    )
+}
---
+++ src/app/components/Main/SectionA.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SectionA: React.FC = () => {
+    return (
+        <section>
+            <h1>Section A</h1>
+            <p>This is the content of Section A.</p>
+        </section>
+    );
+};
